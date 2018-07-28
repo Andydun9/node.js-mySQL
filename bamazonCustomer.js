8c97d119da2721ac6d665dd7862164d600b941f9
Andydun9 <33610842+Andydun9@users.noreply.github.com>
2018-07-28 09:51:39 -0700

    Add files via upload

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,28 +1,46 @@
 var mysql = require("mysql");
+// const cTable = require("console.table");
+const cTable = require('console.table');
+var inquirer = require("inquirer");
+
+
+
 
 var con = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // Your port; if not 3306
-    port: 8889,
+  // Your port; if not 3306
+  port: 8889,
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "root",
-    database: "bamazon"
+  // Your password
+  password: "root",
+  database: "bamazon"
 });
 
 // pulls data from table and displays it node
 con.connect(function (err) {
-    if (err) throw err;
-    //Select all products and return the result object:
-    con.query("SELECT * FROM products", function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-    });
+  if (err) throw err;
+  //Select all products and return the result object:
+  displayProducts();
 });
+
+function displayProducts() {
+  con.query("SELECT * FROM products", function (err, result, fields) {
+    if (err) throw err;
+    console.table(result);
+
+    let productArray = [];
+    for (let i = 0; i < result.length; i++) {
+      productArray.push(result[i].Product_name);
+    }
+
+    proptCustomer(productArray);
+  });
+
+}
 
 //go over this section with the tutor. 
 //  the following questions:
@@ -36,34 +54,135 @@ con.connect(function (err) {
 //     console.log('qty: '+result.qty)
 // }
 
-var inquirer = require("inquirer");
-
-// constructor function used to create programmers objects
-function Programmer(product, qty) {
-  this. product = product;
-  this.qty = qty;
-}
-
-// creates the printInfo method and applies it to all programmer objects
-Programmer.prototype.printInfo = function() {
-  console.log("product: " + this.product + "\nqty: " + this.qty);
-};
 
 
-inquirer.prompt([
+
+
+
+
+function proptCustomer(products) {
+  
+
+  inquirer.prompt([
     {
       name: "product",
-      message: "What product would you like to purchase?"
-    }, 
+      message: "What product would you like to purchase?",
+      type: "list",
+      choices: products
+    },
     {
       name: "qty",
-      message: "How much would you like to purchase?"
+      message: "How much would you like to purchase?",
+      type: "input"
     }
-  ]).then(function(answers) {
+  ]).then(function (answers) {
+
+    // console.log(answers);
     // initializes the variable newProgrammer to be a programmer object which will take
     // in all of the user's answers to the questions above
-    var newProgrammer = new Programmer(answers.product, answers.qty);
+
+    if (Number.isInteger(parseInt(answers.qty))) {
+
+      
+      // constructor function used to create programmers objects
+      function Customer(product, qty) {
+        this.product = product;
+        this.qty = qty;
+        this.itemPurchased = [];
+        
+        
+      }
+
+      // creates the printInfo method and applies it to all programmer objects
+      Customer.prototype.printInfo = function () {
+        console.log("product: " + this.product + "\nqty: " + this.qty);
+      };
+
+      var customer = new Customer(answers.product, answers.qty);
+      checkInventory(customer);
+    } else {
+      console.log("Please enter a valid integer for product quanity");
+      continueShoppingPrompt();
+    }
+
     // printInfo method is run to show that the newProgrammer object was successfully created and filled
-    newProgrammer.printInfo();
+    // newProgrammer.printInfo();
+
+    // console.log(newProgrammer);
+
+
   });
-  
+
+}
+
+function checkInventory(CustomerObject) {
+  con.query("SELECT Product_name, price, Stock_quantity FROM products where Product_name=?",
+    [CustomerObject.product], function (err, results, fields) {
+
+      if (err) throw err;
+
+      // console.log(results);
+
+      let inventory = results[0].Stock_quantity;
+      // console.log("inventory: " + inventory);
+
+      let price = results[0].price;
+      // console.log("Price: " + price);
+
+      let totalSalesPrice = CustomerObject.qty * price;
+      // console.log("totalSalesPrice: " + totalSalesPrice);
+
+      let remainingInventory = inventory - CustomerObject.qty;
+      // console.log("remainingInvetory: " + remainingInventory);
+
+      let productName = CustomerObject.product;
+
+      if (remainingInventory > 0) {
+        console.log("Congrats, you just purchased " + CustomerObject.qty + " " + CustomerObject.product + " for $" + totalSalesPrice);
+        CustomerObject.itemPurchased.push(productName);
+        updateInventoy(productName, remainingInventory)
+
+      } else {
+        console.log("Sorry, insufficient quantity");
+        continueShoppingPrompt();
+      }
+
+    })
+}
+
+function updateInventoy(product, newInventory) {
+  con.query("UPDATE products SET ? WHERE ?", [
+    {
+      Stock_quantity: newInventory
+    },
+    {
+      Product_name: product
+    }
+  ], function (err, results, fields) {
+    if (err) throw err;
+
+    // console.log(results);
+
+    if (results.affectedRows > 0) {
+      continueShoppingPrompt();
+    } else {
+      console.log("Something went wrong! Database wasn't updated. Cancel the sale immediately!");
+      continueShoppingPrompt();
+    }
+  })
+}
+
+function continueShoppingPrompt() {
+  inquirer.prompt([{
+    name: "continue",
+    type: "confirm",
+    message: "Would you like to continue shopping?"
+  }]).then(answers => {
+    if (answers.continue) {
+      displayProducts();
+    } else {
+      console.log("Thank you for shopping! Please come back soon.");
+      con.end();
+    }
+  })
+}
